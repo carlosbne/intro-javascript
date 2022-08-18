ada4648baebd77a4ae9769487d9c5de6295dea22
carlosbne <carlosbneto72@gmail.com>
2022-08-18 19:00:15 -0300

    cocódigos sobre variaveis e conversoes

---
+++ variaveis/conversoes.js
@@ -0,0 +1,14 @@
+//tipo de dados
+//booleanos
+
+//conversão implícita
+const numero = 456;
+const numeroString = '456a';
+
+//Number()
+//String
+
+
+console.log(numero + Number(numeroString));
+
+//conversão explícita
---
+++ variaveis/truthy-falsy.js
@@ -0,0 +1,23 @@
+//boolean
+const usuarioLogado = true;
+const contapaga = false;
+
+//truthy ou falsy
+// 0 => false
+// 1 => true
+
+/* 
+console.log(0 == false);
+console.log('' == false);
+console.log(1 == true); */
+
+// null ==> Vazio ou nada;
+// undefined
+let minhaVar;
+let varNull = null;
+
+console.log(typeof minhaVar);
+console.log(typeof varNull);
+
+let numero = 3;
+let texto = 'Alura';
